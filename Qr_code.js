e2e9e9189b0e46d08038896c9a8d8d0e9892866d
Somesh-tiw <someshtiwari457@gmail.com>
2025-07-13 11:43:05 +0530

    second Commit

--- Qr_code.js
+++ Qr_code.js
@@ -9,3 +9,17 @@ let button=document.querySelector('button')
     }
   
   }
+  function downloadQR() {
+  if (qrText.value.length > 0) {
+    const imageURL = qrImage.src;
+
+    const link = document.createElement('a');
+    link.href = imageURL;
+    link.download = 'qr-code.png'; // file name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  } else {
+    alert("Please enter some text or URL to generate QR code first.");
+  }
+}
